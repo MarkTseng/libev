798cc04a6cb5b53bcdc897b9697d5d41980e822f
root <root>
2007-12-09 15:06:21 +0000

    *** empty log message ***

--- ev_kqueue.c
+++ ev_kqueue.c
@@ -153,7 +153,7 @@ kqueue_init (EV_P_ int flags)
 
   fcntl (backend_fd, F_SETFD, FD_CLOEXEC); /* not sure if necessary, hopefully doesn't hurt */
 
-  backend_fudge  = 1e-3; /* needed to compensate for kevent returning early */
+  backend_fudge  = 0.;
   backend_modify = kqueue_modify;
   backend_poll   = kqueue_poll;
 
